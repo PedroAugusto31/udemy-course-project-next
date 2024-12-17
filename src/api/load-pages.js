67fd9cf13aaf3a1f982ced65cec8c5d3801184a8
Pedro Augusto <pedroaugustoa47@gmail.com>
2024-12-17 11:49:51 -0300

    fixing bug

--- src/api/load-pages.js
+++ src/api/load-pages.js
@@ -3,8 +3,8 @@ import config from "../config";
 
 export const loadPages = async (slug = "") => {
   const cleanSlug = slug ? `?slug=${slug.replace(/[^a-z0-9-_]/gi, "")}` : "";
-  const url = `${config.url}/pages/${cleanSlug}`;
-  // const url = `${config.url}`;
+  // const url = `${config.url}/pages/${cleanSlug}`;
+  const url = `${config.url}`;
 
   const rawData = await fetch(url);
   const json = rawData.json();
